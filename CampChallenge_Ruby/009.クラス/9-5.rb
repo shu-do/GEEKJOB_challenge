704896c71800ac11b9881955aa6b43f2c6a5e5ed
shu-do <ngs91free@gmail.com>
2017-10-10 18:35:39 +0900

    Rubyクラス9-5の提出

---
+++ CampChallenge_Ruby/009.クラス/9-5.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+require './costmor.rb'
+costmor = Costmor.new
+text = costmor.profile
+print(costmor.profile[1..3])
---
+++ CampChallenge_Ruby/009.クラス/costmor.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+class Costmor
+  def profile
+      return 11111, "中島秀堂", 19910301, "東京都豊島区"
+  end
+end
