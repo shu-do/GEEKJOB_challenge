12189e3f33d7d0a207fe4a055b1ad93bc596220e
shu-do <ngs91free@gmail.com>
2017-10-10 17:14:11 +0900

    Rubクラス9-2の提出

---
+++ CampChallenge_Ruby/009.クラス/9-2.rb
@@ -0,0 +1,4 @@
+require './num.rb'
+num = Num.new
+num.number(4)
+num.number(101)
---
+++ CampChallenge_Ruby/009.クラス/num.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+class Num
+  def number(i)
+    if i % 2 == 0
+      print("#{i}は偶数です")
+    else
+      print("#{i}は奇数です")
+    end
+  end
+end
