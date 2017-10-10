58a9d0258b3062f698ea64f92a21f60e2e6c6dbe
shu-do <ngs91free@gmail.com>
2017-10-10 17:13:49 +0900

    Rubクラス9-1の提出

---
+++ CampChallenge_Ruby/009.クラス/9-1.rb
@@ -0,0 +1,5 @@
+require './profile.rb'
+profile = Profile.new
+10.times do |i|
+  profile.hello
+end
---
+++ CampChallenge_Ruby/009.クラス/profile.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+class Profile
+  def hello
+    print("中嶋秀堂\n" , "1991/3/1\n" , "ゲーム好きです\n")
+    return true
+  end
+end
